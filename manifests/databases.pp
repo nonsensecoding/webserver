e9fafc972190b6490b38c9ee6974805c6fac389b
Stefan Harlander <harlander-stefan@web.de>
2015-07-07 08:24:26 +0200

    remove drx specifics like package module dependency

--- manifests/databases.pp
+++ manifests/databases.pp
@@ -3,19 +3,17 @@
 # hiera user interface to call database via hiera
 #
 define webserver::databases {
-
   $database_hash = hiera_hash('database')
   $database_data = $database_hash[$name]
-
-  $ensure     = $database_data['ensure']
-  $user       = $database_data['user']
-  $password   = $database_data['password']
-  $port       = $database_data['port']
-  $host       = $database_data['host']
-  $mysql_perl = $database_data['mysql_perl']
-  $mysql_java = $database_data['mysql_java']
-  $mysql_ruby = $database_data['mysql_ruby']
-  $mysql_php  = $database_data['mysql_php']
+  $ensure        = $database_data['ensure']
+  $user          = $database_data['user']
+  $password      = $database_data['password']
+  $port          = $database_data['port']
+  $host          = $database_data['host']
+  $mysql_perl    = $database_data['mysql_perl']
+  $mysql_java    = $database_data['mysql_java']
+  $mysql_ruby    = $database_data['mysql_ruby']
+  $mysql_php     = $database_data['mysql_php']
 
   if $database_data['provider'] {
     $provider = $database_data['provider']
